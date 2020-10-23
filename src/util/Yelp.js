7ebc37aa3be575aa52c87dbe28ecee028fdb4a56
Esther Siebert <esther_siebert@live.nl>
2020-10-23 16:05:51 +0200

    Ravenous 4.14 done and working

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,3 +1,15 @@
 const apiKey = '6jfZjt4FQkyAPARzvsRqUxg6obETDTEjD6qXdva_MttI3qCm5tByzBke1onGfW0HEh4_OBMoUSAbg2EF-cNeSepxK9rYbW9d5KgEado6oKv8IweWfTyzQ_sZ0luQX3Yx';
 
-const Yelp = {};
+const Yelp = {
+
+    search(term, location, sortBy) {
+        return fetch(
+            `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+            { 
+                headers: { Authorization: `Bearer ${apiKey}` } 
+            }).then(response => {
+                return response.json();
+            });
+    }
+};
+
